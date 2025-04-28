4be48fe92c35ae20ab67c864a367d9b59f76e7b8
CompactGit <compactgit@gmail.com>
2025-04-28 16:09:28 -0500

    separacion carrito

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -9,7 +9,7 @@ export default function CartPage() {
     return (
         <>
             <Navbar />
-            <main className="pt-[122px]">
+            <main className="pt-[50px]">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
                     <Cart />
                 </div>
